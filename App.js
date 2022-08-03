9755e774c1f50bbf88bb7358d7dda17bc559515c
Joao lucas <joao.freitas@icomp.ufam.edu.br>
2022-08-03 18:42:43 -0400

    Modalize test only for learning

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
+import { Modalize } from 'react-native-modalize';
 
-export default function App() {
+const App = () => {
+  const modalizeRef = useRef(null);
+
+  const onOpen = () => {
+    modalizeRef.current?.open();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View>
+        <Text>Welcome to my dinamic modal test</Text>
+      </View>
+      <TouchableOpacity onPress={onOpen} style={{ backgroundColor: '#cacaca', padding: 10, borderRadius: 10, margin: 15 }}>
+        <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Open the modal</Text>
+      </TouchableOpacity>
+
+      <Modalize 
+        ref={modalizeRef}
+        snapPoint={180}
+        modalHeight={180}
+      >
+        <View style={{ flex: 1, height: 180, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}>
+          <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center', borderRadius: 8,
+        padding: 15, backgroundColor: '#29ae19' }}>
+            <Text>EDITAR</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center', borderRadius: 8,
+        padding: 15, backgroundColor: '#ff0000' }}>
+            <Text>EXCLUIR</Text>
+          </TouchableOpacity>
+
+        </View>
+      </Modalize>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default gestureHandlerRootHOC(App);
